facef02f0100bbcc1c5eca164e0ca6ea563a8c91
Fleny113 <Fleny113@outlook.com>
2021-04-25 16:07:27 +0200

    initial files

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-import flask, gevent.pywsgi
-import os, glob, importlib
+import flask
+import gevent.pywsgi
+import os
+import glob
+import importlib
 #import flask, pymongo, validators, string, random, os, glob, importlib
 
 app = flask.Flask("Fasmga")
 
+
 @app.route("/testm")
 def testm():
-	print("this is a test!")
+    print("this is a test!")
 
-
-for filename in [os.path.basename(f)[:-3] for f in glob.glob(os.path.join(os.path.dirname("./Sources/Python/"), "*.py")) if os.path.isfile(f)]:
-	filename = filename.removesuffix(".py")
-	module = importlib.import_module(f"Sources.Python.{filename}")
-	module.setup(app)
 
 if (os.environ.get("FLASK_ENV") == "development"):
-	app.run()
+    app.run()
 else:
-	http_server = gevent.pywsgi.WSGIServer(('0.0.0.0', 5000), app)
-	http_server.serve_forever()
-	
+    http_server = gevent.pywsgi.WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
+
+for filename in [os.path.basename(f)[:-3] for f in glob.glob(os.path.join(os.path.dirname("./Sources/Python/"), "*.py")) if os.path.isfile(f)]:
+    filename = filename.removesuffix(".py")
+    module = importlib.import_module(f"Sources.Python.{filename}")
+    module.setup(app)
