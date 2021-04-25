e94b86c650f3fbc792da9ed4d712b849c723ab60
Fleny113 <Fleny113@outlook.com>
2021-04-25 17:30:45 +0200

    test

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-import flask, gevent.pywsgi
+from flask import Flask, render_template
+import gevent.pywsgi, pymongo
 import os, glob, importlib
-#import flask, pymongo, validators, string, random, os, glob, importlib
 
-app = flask.Flask("Fasmga")
+app = Flask("Fasmga", template_folder='Sources/HTML')
+app.database = pymongo.MongoClient(os.getenv("MongoDBString"))
+app.database.db = app.database["Fasmga"]
 
-@app.route("/testm")
-def testm():
-	print("this is a test!")
+@app.route('/')
+def main():
+  return render_template("index.html")
 
+for filename in [os.path.basename(f)[:-3] for f in glob.glob(os.path.join(os.path.dirname("./Sources/Python/"), "*.py")) if os.path.isfile(f)]:
+    filename = filename.removesuffix(".py")
+    module = importlib.import_module(f"Sources.Python.{filename}")
+    module.setup(app)
 
 if (os.environ.get("FLASK_ENV") == "development"):
-	app.run()
+    app.run()
 else:
-	http_server = gevent.pywsgi.WSGIServer(('0.0.0.0', 5000), app)
-	http_server.serve_forever()
-	
-for filename in [os.path.basename(f)[:-3] for f in glob.glob(os.path.join(os.path.dirname("./Sources/Python/"), "*.py")) if os.path.isfile(f)]:
-	filename = filename.removesuffix(".py")
-	module = importlib.import_module(f"Sources.Python.{filename}")
-	module.setup(app)
+    http_server = gevent.pywsgi.WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
