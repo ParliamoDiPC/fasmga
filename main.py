1f1411b1e4b7ab20d85c672dbec14b0f48905134
Fleny113 <Fleny113@outlook.com>
2021-04-27 11:01:11 +0200

    Add html files and url for passwordProtect image

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template, send_from_directory
-import gevent.pywsgi, pymongo
+import gevent.pywsgi, pymongo, dotenv
 import os, glob, importlib
 
-app = Flask("Fasmga", template_folder='Sources/HTML')
+dotenv.load_dotenv()
+
+app = Flask("Fasmga", template_folder="Sources/HTML")
 app.database = pymongo.MongoClient(os.getenv("MongoDBString"))
 app.database.db = app.database["Fasmga"]
 
 @app.route("/")
 def main():
-  return render_template("main.html")
+  return render_template("main.html", login=False)
 
 @app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'Sources/Image'),'favicon.ico',mimetype='image/vnd.microsoft.icon')
+  return send_from_directory(os.path.join(app.root_path, "Sources/Image"),"favicon.ico", mimetype="image/vnd.microsoft.icon")
+
+@app.route("/sources/image/passwordProtect.png")
+def password_protect_image():
+	return send_from_directory(os.path.join(app.root_path, "Sources/Image"),"password_protected.png")
 
 for filename in [os.path.basename(f)[:-3] for f in glob.glob(os.path.join(os.path.dirname("./Sources/Python/"), "*.py")) if os.path.isfile(f)]:
 	filename = filename.removesuffix(".py")
@@ -24,7 +30,7 @@ if (os.environ.get("FLASK_ENV") == "development"):
 else:
 	try:
 		host = os.getenv("host") or "0.0.0.0"
-		port = os.getenv("port") or 5000
+		port = int(os.getenv("port")) or 5000
 		print(f"Running on http://{host}:{port}")
 		http_server = gevent.pywsgi.WSGIServer((host, port), app)
 		http_server.serve_forever()
